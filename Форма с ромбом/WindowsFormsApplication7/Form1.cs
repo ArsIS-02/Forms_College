e59017722016383d7fa698891617b2611de26f38
ArsIS-02 <74455553+ArsIS-02@users.noreply.github.com>
2021-06-18 10:15:55 +0500

    Add files via upload

---
+++ Форма с ромбом/WindowsFormsApplication7/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // Задаем цвет прозрачности
+            this.TransparencyKey = Color.Gold;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(Color.Gold);
+            e.Graphics.FillPolygon(Brushes.Aquamarine, new PointF[] {
+new Point(this.ClientRectangle.Width / 2, 0),
+new Point(this.ClientRectangle.Width,
+this.ClientRectangle.Height / 2),
+new Point(this.ClientRectangle.Width / 2,
+this.ClientRectangle.Height),
+new Point(0, this.ClientRectangle.Height / 2)});
+            Font f = new Font(this.Font.Name, 20);
+            e.Graphics.DrawString("C#.Народные советы",
+            f, Brushes.Black, 20,
+            (this.ClientRectangle.Height / 2) - (f.Height / 2));
+
+        }
+    }
+}
---
+++ Форма со строкой для ввода/WindowsFormsApplication11/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            AutoCompleteStringCollection source = new AutoCompleteStringCollection()
+{
+"Кузнецов",
+"Иванов",
+"Петров",
+"Кустов"
+};
+            textBox1.AutoCompleteCustomSource = source;
+            textBox1.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            textBox1.AutoCompleteSource = AutoCompleteSource.CustomSource;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
