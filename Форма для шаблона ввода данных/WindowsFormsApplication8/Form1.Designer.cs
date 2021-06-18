01efde851c794f46e813c9df8e11ddc51bd9138f
ArsIS-02 <74455553+ArsIS-02@users.noreply.github.com>
2021-06-18 11:41:58 +0500

    Add files via upload

---
+++ Форма для шаблона ввода данных/WindowsFormsApplication8/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace WindowsFormsApplication8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AutoCompleteCustomSource.AddRange(new string[] {
+            "Кузнецов,",
+            "Иванов,",
+            "Петров,",
+            "Кустов"});
+            this.textBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.textBox1.Location = new System.Drawing.Point(267, 88);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Фамилия";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(168, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Имя";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(288, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Отчество";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(134, 88);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 87);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 432);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+
+    }
+}
+
---
+++ Форма для шаблона ввода данных/WindowsFormsApplication8/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            AutoCompleteStringCollection source1 = new AutoCompleteStringCollection()
+          {
+            "Кузнецов",
+             "Иванов",
+             "Петров",
+             "Кустов",
+             "Борисов",
+             "Яковлев",
+            "Григорьев",
+            "Романов",
+            "Воробьев",
+            "Сергеев",
+            "Кузьмин",
+            "Фролов",
+            "Пупкин",
+            "Чехлов",
+            "Зарубкин",
+            "Шишкин",
+            "Кочерыжкин",
+            "Ёлкин",
+            "Дрынов",
+            "Петров",
+            "Дынин",
+            "Кукушкин",
+            "Смирнов",
+            "Лысенко",
+            "Пономарёв",
+            "Палкин",
+            "Сидоров",
+            "Плюшкин",
+            "Червоный",
+            "Печкин",
+
+
+
+           };
+            textBox2.AutoCompleteCustomSource = source1;
+            textBox2.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            textBox2.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            AutoCompleteStringCollection source2 = new AutoCompleteStringCollection()
+          {
+             "Иван",
+             "Петр",
+             "Илья",
+             "Максим",
+             "Борис",
+             "Яков",
+            "Григорий",
+            "Роман",
+            "Игорь",
+            "Сергей",
+            "Кузьма",
+            "Федор",
+            "Вадим",
+            "Алексей",
+            "Дмитрий",
+            "Павел",
+            "Юрий",
+            "Константин",
+            "Василий",
+            "Ян",
+            "Анатолий",
+            "Николай",
+            "Антон",
+            "Валера",
+            "Семен",
+            "Владимир",
+            "Сидор",
+            "Артём",
+            "Павел",
+            "Данил"
+
+
+           };
+            textBox3.AutoCompleteCustomSource = source2;
+            textBox3.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            textBox3.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            AutoCompleteStringCollection source3 = new AutoCompleteStringCollection()
+          {
+            "Иванович",
+             "Петрович",
+             "Ильич",
+             "Максимович",
+             "Борисович",
+             "Яковлевич",
+            "Григорьевич",
+            "Романович",
+            "Игоревич",
+            "Сергеич",
+            "Кузьмич",
+            "Федорович",
+            "Вадимович",
+            "Алексеич",
+            "Дмитриевич",
+            "Павлович",
+            "Юрьевич",
+            "Константинович",
+            "Васильевич",
+            "Янович",
+            "Анатольевич",
+            "Николаевич",
+            "Антонович",
+            "Валерьевич",
+            "Семенович",
+            "Владимирович",
+            "Сидорович",
+            "Артёмович",
+            "Павлович",
+            "Данилович"
+
+
+           };
+            textBox1.AutoCompleteCustomSource = source3;
+            textBox1.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            textBox1.AutoCompleteSource = AutoCompleteSource.CustomSource;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
